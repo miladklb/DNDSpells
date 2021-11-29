09404ca272af7e7fa1765d142ced021166706a40
dino-inc <ethan8960@gmail.com>
2021-11-29 10:32:43 -0800

    Removed colinmarc's personal website

--- deploy.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-(cd dndspells && echo "var spells = " | cat - spells.json | minify --type js > spells.js)
-
-ssh nessus "sudo rm -rvf /var/www/*"
-files=$(find . -name "*.js" -or -name "*.html" -or -name "*.css" -or -name "*.woff*" -or -name "*.jpg")
-tar cz $files | ssh nessus "sudo tar xvz --no-same-owner -C /var/www"
