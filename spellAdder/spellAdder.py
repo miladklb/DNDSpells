faf1c67ca253a4b2bcdbab90c760db0244eff8a8
dino-inc <ethan8960@gmail.com>
2021-12-01 00:32:51 -0800

    Added skeleton of layout

--- spellAdder/spellAdder.py
+++ spellAdder/spellAdder.py
@@ -1,9 +1,107 @@
 from tkinter import *
 from tkinter import ttk
+import tkinter as tk
 root = Tk(className='Add a spell!')
-root.geometry("500x700")
 frm = ttk.Frame(root, padding=10)
 frm.grid()
-ttk.Label(frm, text="Hello World!").grid(column=0, row=0)
-ttk.Button(frm, text="Quit", command=root.destroy).grid(column=1, row=0)
+
+'''
+Label = ttk.Label(frm, text="")
+ = ttk.Entry(frm)
+Label.grid(column=0, row=, sticky="w")
+.grid(column=1, row=, sticky="w")
+'''
+# Name
+nameLabel = ttk.Label(frm, text="Name:")
+name = ttk.Entry(frm)
+nameLabel.grid(column=0, row=0, sticky="w")
+name.grid(column=1, row=0, sticky="w")
+
+# Desc
+descLabel = ttk.Label(frm, text="Description:")
+desc = Text(frm, height=10)
+descLabel.grid(column=0, row=1, sticky="nw")
+desc.grid(column=1, row=1, sticky="w")
+
+# Page
+pageLabel = ttk.Label(frm, text="Page:")
+page= ttk.Entry(frm)
+pageLabel.grid(column=0, row=2, sticky="w")
+page.grid(column=1, row=2, sticky="w")
+
+# Range
+rangeLabel = ttk.Label(frm, text="Range:")
+range = ttk.Entry(frm)
+rangeLabel.grid(column=0, row=3, sticky="w")
+range.grid(column=1, row=3, sticky="w")
+
+# Components
+compLabel = ttk.Label(frm, text="Components:")
+compLabel.grid(column=0, row=4, sticky="w")
+
+componentFrm = ttk.Frame(frm)
+componentFrm.grid()
+componentFrm.grid(column=1, row=4, sticky="w")
+
+verbalVar = tk.IntVar()
+somaticVar = tk.IntVar()
+materialVar = tk.IntVar()
+verbal = ttk.Checkbutton(componentFrm, text="Verbal", variable=verbalVar)
+somatic = ttk.Checkbutton(componentFrm, text="Somatic", variable=somaticVar)
+material = ttk.Checkbutton(componentFrm, text="Material", variable=materialVar)
+verbal.grid(column=0, row=0, sticky="w")
+somatic.grid(column=1, row=0, sticky="w")
+material.grid(column=2, row=0, sticky="w")
+
+# Materials
+materialLabel = ttk.Label(frm, text="Materials:")
+material = ttk.Entry(frm)
+materialLabel.grid(column=0, row=5, sticky="w")
+material.grid(column=1, row=5, sticky="w")
+
+# Ritual
+ritualLabel = ttk.Label(frm, text="Ritual:")
+ritualVar = tk.IntVar()
+ritual = ttk.Checkbutton(frm, variable=ritualVar)
+ritualLabel.grid(column=0, row=6, sticky="w")
+ritual.grid(column=1, row=6, sticky="w")
+
+# Duration
+durationLabel = ttk.Label(frm, text="Duration:")
+duration= ttk.Entry(frm)
+durationLabel.grid(column=0, row=7, sticky="w")
+duration.grid(column=1, row=7, sticky="w")
+
+# Concentration
+concentrationLabel = ttk.Label(frm, text="Concentration:")
+concentrationVar = tk.IntVar()
+concentration = ttk.Checkbutton(frm, variable=concentrationVar)
+concentrationLabel.grid(column=0, row=8, sticky="w")
+concentration.grid(column=1, row=8, sticky="w")
+
+# Casting Time
+castLabel = ttk.Label(frm, text="Cast time:")
+cast = ttk.Entry(frm)
+castLabel.grid(column=0, row=9, sticky="w")
+cast.grid(column=1, row=9, sticky="w")
+
+# Level
+levelVar = tk.IntVar()
+levelLabel = ttk.Label(frm, text="Level:")
+level = ttk.OptionMenu(frm, levelVar, "Choose Level", 0,1,2,3,4,5,6,7,8,9)
+levelLabel.grid(column=0, row=10, sticky="w")
+level.grid(column=1, row=10, sticky="w")
+
+# School
+schoolLabel = ttk.Label(frm, text="School:")
+school = ttk.Entry(frm)
+schoolLabel.grid(column=0, row=11, sticky="w")
+school.grid(column=1, row=11, sticky="w")
+
+# Class
+classLabel = ttk.Label(frm, text="Class:")
+charClass = ttk.Entry(frm)
+classLabel.grid(column=0, row=12, sticky="w")
+charClass.grid(column=1, row=12, sticky="w")
+
 root.mainloop()
