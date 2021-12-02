aa4f0c105c8cfa0c70e3fb623650f4e237b6990d
dino-inc <ethan8960@gmail.com>
2021-12-02 00:56:51 -0800

    Removed test commit

--- spells.js
+++ spells.js
@@ -27,41 +27,6 @@ var spells = [
     "source": "ASIS",
     "page": 15
   },
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  {
-    "name": "test commit",
-    "desc": "<p>You draw the moisture from every creature in a 30-foot cube centered on a point you choose within range. Each creature in that area must make a Constitution saving throw. Constructs and undead aren't affected, and plants and water elementals make this saving throw with disadvantage. A creature takes 12d8 necrotic damage on a failed save, or half as much damage on a successful one.</p><p>Nonmagical plants in the area that aren't creatures, such as trees and shrubs, wither and die instantly.</p>",
-    "range": "150 feet",
-    "ritual": false,
-    "duration": "Instantaneous",
-    "concentration": false,
-    "casting_time": "1 action",
-    "level": 8,
-    "school": "Necromancy",
-    "class": [
-      "Sorcerer",
-      "Wizard"
-    ],
-    "level_desc": "8th-level",
-    "class_desc": "Sorcerer, Wizard",
-    "range_desc": "150 feet",
-    "component_desc": "V, S, M",
-    "verbal": true,
-    "material": true,
-    "somatic": true,
-    "material_desc": "a bit of sponge",
-    "material_cost": false,
-    "source": "EE PC",
-    "page": 15
-  },
   {
     "name": "Abi-Dalzim's Horrid Wilting",
     "desc": "<p>You draw the moisture from every creature in a 30-foot cube centered on a point you choose within range. Each creature in that area must make a Constitution saving throw. Constructs and undead aren't affected, and plants and water elementals make this saving throw with disadvantage. A creature takes 12d8 necrotic damage on a failed save, or half as much damage on a successful one.</p><p>Nonmagical plants in the area that aren't creatures, such as trees and shrubs, wither and die instantly.</p>",
