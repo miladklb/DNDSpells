f60e00a3b785281a20ea042f4f1394d8a8cc879f
Ethan <30489196+dino-inc@users.noreply.github.com>
2023-09-26 22:28:23 -0700

    Whack-a-PHB

--- main.js
+++ main.js
@@ -54,7 +54,7 @@ $(document).ready(function() {
       filterOaths = new Set(),
       filterPatrons = new Set(),
 
-      filterSources = new Set(['PHB']),
+      filterSources = new Set(['FNF']),
       filterLevels = new Set(),
       filterSchools = new Set(),
       filterSearch = '';
